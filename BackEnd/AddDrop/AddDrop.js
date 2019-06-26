ebaa9d835197e813ac218a87106bcd151c021a13
seneyd <Broncos30>
2019-06-25 23:40:39 -0400

    autocomplete but not connected to DB

--- BackEnd/AddDrop/AddDrop.js
+++ BackEnd/AddDrop/AddDrop.js
@@ -1,22 +1,32 @@
 const fs = require('fs');
-
 module.exports = {
-	addDropPage: (req, res) => {
-       // let query = "SELECT * FROM `players`"; // query database to get all the Rooms
 
-        // execute query
-       // db.query(query, [1, 2], (err, result) => {
-        //    if (err) {
-          //      res.redirect('/');
-           // }
-            res.render('add_drop.ejs', {
-                
-            });
-        //});
-    },
-	//playerStats: (req, res) => {
-	//	res.render('playerstats.ejs', {
-      //          
-        //    });
-	//}
+  addDropPage: (req, res) => {
+
+    db.query("SELECT p.first_name, p.last_name, p.Player_ID FROM offense_stats o, players p WHERE o.Player_ID = p.player_id and o.Year = 2017 GROUP BY p.player_id", [1,2], (err, result) => {
+
+ 
+
+      if (err) {
+
+        res.redirect('/');
+
+      }
+
+      else{
+
+        //console.log(result); // [{1: 1}]
+
+        }
+
+        res.render('add_drop.ejs', {
+
+           players: result      
+
+        });
+
+      });
+
+      },
+
 }
