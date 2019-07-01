a21cee674a40ca68f71693820ecaceb8507a3bed
seneyd <Broncos30>
2019-06-30 21:42:00 -0400

    autocomplete for add_drop

--- BackEnd/Trade/Trade.js
+++ BackEnd/Trade/Trade.js
@@ -1,22 +1,19 @@
 const fs = require('fs');
-
 module.exports = {
-	tradePage: (req, res) => {
-       // let query = "SELECT * FROM `players`"; // query database to get all the Rooms
 
-        // execute query
-       // db.query(query, [1, 2], (err, result) => {
-        //    if (err) {
-          //      res.redirect('/');
-           // }
-            res.render('trade.ejs', {
-                
-            });
-        //});
-    },
-	//playerStats: (req, res) => {
-	//	res.render('playerstats.ejs', {
-      //          
-        //    });
-	//}
+	tradePage: (req, res) => {
+ 
+    db.query("SELECT p.first_name, p.last_name, p.Player_ID FROM offense_stats o, players p WHERE o.Player_ID = p.player_id and o.Year = 2017 GROUP BY p.player_id", [1,2], (err, result) => {
+
+      if (err) {
+        res.redirect('/');
+      }
+      else{
+        // console.log(result); // [{1: 1}]
+        }
+        res.render('trade.ejs', {
+           players1: result      
+        });
+      });
+      },
 }
